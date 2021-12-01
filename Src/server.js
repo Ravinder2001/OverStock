8fa276cfa7000f09ccb7e374c6fbb304597733fd
Ravinder Singh Negi <rvnegi786@gmail.com>
2021-12-01 17:42:32 +0530

    port

--- Src/server.js
+++ Src/server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const connect = require('./configs/db');
 const path = require('path');
-
+const port = process.env.PORT || 4398;
 const app = express();
 function change() {
 	return hello;
@@ -11,16 +11,16 @@ function change() {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 console.log(__dirname);
-app.set('views', path.join(__dirname, 'views/'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views/"));
+app.set("view engine", "ejs");
 
-app.use('/static', express.static(path.join(__dirname, 'Style')));
-app.use('/static', express.static(path.join(__dirname, 'Script')));
+app.use("/static", express.static(path.join(__dirname, "Style")));
+app.use("/static", express.static(path.join(__dirname, "Script")));
 
-const homeController = require('./controllers/home.controller');
-app.use('/home', homeController);
+const homeController = require("./controllers/home.controller");
+app.use("/home", homeController);
 
-app.listen(4398, async () => {
+app.listen(port, async () => {
 	await connect();
-	console.log('listening to port 4321');
+	console.log("listening to port 4321");
 });
