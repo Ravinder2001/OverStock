76189944fbf8698730af81ac401f5d4a4d0c6531
Ravinder Singh Negi <rvnegi786@gmail.com>
2021-12-01 17:50:38 +0530

    src

--- Src/server.js
+++ Src/server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const connect = require('./configs/db');
 const path = require('path');
-const port = process.env.PORT || 4398;
+const port = process.env.PORT || 4000;
 const app = express();
 function change() {
 	return hello;
@@ -18,7 +18,7 @@ app.use("/static", express.static(path.join(__dirname, "Style")));
 app.use("/static", express.static(path.join(__dirname, "Script")));
 
 const homeController = require("./controllers/home.controller");
-app.use("/home", homeController);
+app.use("/", homeController);
 
 app.listen(port, async () => {
 	await connect();
